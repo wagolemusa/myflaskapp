7078fbd470a2e381ace3c9da5b30cce59981ffbd
kisumutalent <homiemusa@gmail.com>
2018-04-25 09:57:39 +0300

    creating data file

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
+from data import Articles
+
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def home():
@@ -9,6 +13,15 @@ def home():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+	return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+	return render_template('articles.html', id=id)
 
 
 if __name__ == '__main__':
---
+++ data.py
@@ -0,0 +1,34 @@
+def Articles():
+	articles = [
+
+			{
+
+				'id' : 1,
+				'title' : 'Article one',
+				'body'  : 'Now ParseHub will call this handler every time the status of a run changes. It doesnt matter how long the run takes, or if it queued up on our servers for a while. Itll just work',
+				'auther': 'Refuge',
+				'create_date' : '04-24-2018'
+			},
+
+
+				{
+
+				'id' : 1,
+				'title' : 'Article Two',
+				'body'  : 'Now ParseHub will call this handler every time the status of a run changes. It doesnt matter how long the run takes, or if it queued up on our servers for a while. Itll just work',
+				'auther': 'Wise',
+				'create_date' : '04-26-2018'
+			},
+
+
+				{
+
+				'id' : 1,
+				'title' : 'Article Three',
+				'body'  : 'Now ParseHub will call this handler every time the status of a run changes. It doesnt matter how long the run takes, or if it queued up on our servers for a while. Itll just work',
+				'auther': 'Homie',
+				'create_date' : '04-30-2018'
+			}
+
+	]
+	return articles
