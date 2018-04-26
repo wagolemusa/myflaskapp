596a58b822a6f77d6f34ce050a528df609db5424
kisumutalent <homiemusa@gmail.com>
2018-04-26 08:56:01 +0300

    setting up registration form

--- data.py
+++ data.py
@@ -13,9 +13,9 @@ def Articles():
 
 				{
 
-				'id' : 1,
+				'id' : 2,
 				'title' : 'Article Two',
-				'body'  : 'Now ParseHub will call this handler every time the status of a run changes. It doesnt matter how long the run takes, or if it queued up on our servers for a while. Itll just work',
+				'body'  : 'My Kickstarter project was a big success! Im now releasing a chapter of the new and improved Flask Mega-Tutorial every Tuesday here on this blog!',
 				'auther': 'Wise',
 				'create_date' : '04-26-2018'
 			},
@@ -23,9 +23,9 @@ def Articles():
 
 				{
 
-				'id' : 1,
+				'id' : 3,
 				'title' : 'Article Three',
-				'body'  : 'Now ParseHub will call this handler every time the status of a run changes. It doesnt matter how long the run takes, or if it queued up on our servers for a while. Itll just work',
+				'body'  : 'I have also created ebook and video versions of the complete tutorial, which Im offering for sale. Click on the book cover below for more information!',
 				'auther': 'Homie',
 				'create_date' : '04-30-2018'
 			}
